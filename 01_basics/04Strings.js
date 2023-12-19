ebba90dda57fc6e5b5245be90f2c8772c8d2b628
Bhavya Raj Joshi <149313618+BhavyaRajJoshi@users.noreply.github.com>
2023-12-19 01:50:02 +0000

    lec10and11

---
+++ 01_basics/04Strings.js
@@ -0,0 +1,35 @@
+
+//stack and heap memory
+// Stack -- primitive
+
+// Heap -- non primitive
+
+// stack -- small small blocks
+
+// heap -- one huge container 
+
+//stack -- copy reference || Heap -- direct value. 
+
+//////////// || || || || || || || || || || || || || || || \\\\\\\\\\\\\\
+
+const name = "hitesh"
+const repoCount = 50
+
+//console.log(name + repoCount + " value")
+
+console.log(`Hello my name is ${name} and my value is ${repoCount}`)
+
+
+const gameName = new String('hite shhc')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+const newString = gameName.substring(0, 4)
+
+console.log(newString)
+
+console.log(gameName.split(' '))
---
+++ 01_basics/06_NumsAndMath.js
@@ -0,0 +1,10 @@
+const balance = new Number(100)
+
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 13.89
+
+console.log(otherNumber.toPrecision(1))
