b915d46ee67b430ea98d9a8c87f489bd67529e06
Bhavya Raj Joshi <149313618+BhavyaRajJoshi@users.noreply.github.com>
2023-12-20 01:40:55 +0000

    date and time

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,29 @@
+//dates
+
+let myDate = new Date()
+
+// console.log(myDate.toString())
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toLocaleString())
+
+// console.log(myDate)
+
+// console.log(typeof myDate)
+
+let myCreatedDate = new Date(2023,1,23)
+
+//console.log(myCreatedDate.toDateString())
+
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp) //in miliseconds
+
+//console.log(Math.floor(Date.now()/1000)) // to get date in seconds and also round off
+
+let newDate = new Date()
+
+console.log(newDate)
+console.log(newDate.getDay())
